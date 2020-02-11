cc5feb093041a3b6b40c5358ec375ee1d170d0df
Lucas Webb <lucasawebb@gmail.com>
2020-02-10 17:40:59 -0700

    Changes

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-=======
+
 #include <vector>
 #include <string>
->>>>>>> master
 
 /* String functions section */
 
@@ -135,16 +133,3 @@ std::vector<std::string> AddN(std::vector<std::string>, std::string n);
 
 // subtracts n to each element of the vector
 std::vector<int> SubtractN(std::vector<int>, int n);
-
-// subtracts n to each element of the vector
-<<<<<<< HEAD
-std::vector<double> SubtractN(std::vector<double>, double n);
-
-
-TEST_CASE( "Sign function works", "[wign]" ) {
-    REQUIRE( Sign(1) == 1 );
-    REQUIRE( Sign (-1) == -1 );
-}
-=======
-std::vector<double> SubtractN(std::vector<double>, double n);
->>>>>>> master
